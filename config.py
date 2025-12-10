52f7b9f0aab5233f005ed388c58b65e664cca0c4
nexo81878-ux <nexo81878@gmail.com>
2025-12-11 00:22:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(environ.get("API_ID", "15090477"))
 API_HASH = environ.get("API_HASH", "2d81a0d94f0d111339c455a85f60e5c8")
 
 # Your Main Bot Token 
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8524698471:AAEFavlhNg4Nrbibgoesftrwb1_Mr-1G13s")
 
 # Owner ID For Broadcasting 
 OWNER_ID = int(environ.get("OWNER_ID", "1715252294")) # Owner Id or Admin Id
