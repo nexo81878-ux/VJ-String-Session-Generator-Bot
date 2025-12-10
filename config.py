d8463f39fb6f6431548ac1e2952e46b340be7271
nexo81878-ux <nexo81878@gmail.com>
2025-12-10 23:34:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from os import environ
 
 # Telegram Account Api Id And Api Hash
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "15090477"))
+API_HASH = environ.get("API_HASH", "2d81a0d94f0d111339c455a85f60e5c8")
 
 # Your Main Bot Token 
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
@@ -14,7 +14,7 @@ OWNER_ID = int(environ.get("OWNER_ID", "")) # Owner Id or Admin Id
 F_SUB = environ.get("F_SUB", "")
 
 # Mongodb Database Uri For User Data Store 
-MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
+MONGO_DB_URI = environ.get("MONGO_DB_URI", "mongodb+srv://xtrnexo:<db_password>@xtrnexo.b1ioawy.mongodb.net/?appName=xtrnexo")
 
 # Port To Run Web Application 
 PORT = int(environ.get('PORT', 8080))
